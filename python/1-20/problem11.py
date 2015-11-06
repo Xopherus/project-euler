af431057a4c1b47bbad6f004e278db2bd03d148d
Chris Raborg <craborg1@umbc.edu>
2015-11-05 19:27:38 -0500

    modified python directory structure

--- python/1-20/problem11.py
+++ python/1-20/problem11.py
@@ -38,7 +38,7 @@ def get_horizontal_chunks(grid, window):
 
 
 def main():
-    gridStr = """
+    grid_str = """
         08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
         49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
         81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
@@ -61,7 +61,7 @@ def main():
         01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
     """
     grid = []
-    for line in gridStr.split('\n'):
+    for line in grid_str.split('\n'):
         if line.strip():
             grid.append([int(num) for num in line.strip().split(' ')])
 
